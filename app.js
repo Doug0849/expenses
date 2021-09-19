57cb83bbac9917c776cff01a5916848582f9fb3b
doug0849 <etaomonk@gmail.com>
2021-09-19 18:00:29 +0700

    feat: 初始化檔案資料，尚未加上Mongoose連線MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require ('express')
+const express = require('express')
 const exphbs = require('express-handlebars')
 
+const routes = require('./routes')
+
 const app = express()
-const port = 3000
+const PORT = 3000
 
-// 第一個參數為副檔名
-app.engine('hbs', exphbs)
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-app.get('/', (req,res) => {
-  res.send(``)
+app.use(routes)
+
+app.listen(PORT,() => {
+  console.log('Server is running on http://localhost:3000')
 })
-
