7caaed88678647da704d3f912b0ba290b2ceb04d
doug0849 <etaomonk@gmail.com>
2021-09-19 22:38:36 +0700

    feat: 將檔案從伺服器引入HTML成功

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Expenses = require('../../models/expense')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Expenses.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(expensesItems => res.render('index', { expensesItems }))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
